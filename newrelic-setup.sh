eb7e60cd3e8b81611ef5d7563407dbaa9701c521
Mike Bruzina <mbruzina@newrelic.com>
2023-05-26 09:59:33 -0500

    fixing dir typo

--- newrelic-setup.sh
+++ newrelic-setup.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install unzip
 
 # download java agent
 curl -O https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic-java.zip
-
 unzip newrelic-java.zip
+
+# move into project, just for testing
 mv newrelic sample-rest-api/.newrelic
