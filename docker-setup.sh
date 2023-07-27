512fa488e1dab501604e156e608416007aef8cd9
Mike Bruzina <90725916+mbruzina@users.noreply.github.com>
2023-07-27 11:32:30 -0500

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -5,7 +5,7 @@ cd $HOME
 sudo apt install -y default-jre
 
 # clone repo & build executable jar
-git clone https://github.com/mbruzina/sample-rest-api.git $$ \
+git clone https://github.com/mbruzina/sample-rest-api.git && \
 cd sample-rest-api/ && \
 ./gradlew bootJar # or ./gradlew build for a war
 
